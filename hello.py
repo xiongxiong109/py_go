615c12b58126924793ae1d5c0e085454416031a9
xjq熊剑桥 <jqxiong@Ctrip.com>
2018-03-21 10:47:26 +0800

    condition

--- hello.py
+++ hello.py
@@ -1,4 +1,17 @@
 # print hello
-print(100 + 200)
+# print(100 + 200)
 # 多个参数之间会用空格隔开
-print('hello', 'world')
+# print('hello', 'world')
+
+# 使用input获取用户的输入
+# name = input()
+# print('hello', name)
+
+# 条件判断(py 用缩进换行)
+print('use esLint ?(Y/n)')
+isUse = input()
+
+if isUse == 'Y':
+    print('ok')
+else:
+    print('end')
