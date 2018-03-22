7001bd56e30abd46b62abf8b7057701c5ae68cf7
xjq熊剑桥 <jqxiong@Ctrip.com>
2018-03-22 10:22:28 +0800

    update str

---
+++ str.py
@@ -0,0 +1,28 @@
+# 引号转义问题
+# print('I\'m', 'Bear');
+# 用r''表示字符串都不转义
+# print(r"I'm Bear \t Carmbridge ");
+# print("I'm \n Bear \t Carmbridge ");
+
+# 多重换行
+# print('''hello
+# I 'm Bear "Xiong"''')
+
+# Boolean (and, or, not)
+
+# print(True or False)
+# print(True and False)
+# print(not False) # 非
+
+# a = 1
+
+# if not a:
+#     print('ok')
+# else:
+#     print('hello')
+
+# 浮点除法与地板除法
+# print(10 / 3)
+# 地板除, 永远取整数
+# print(10 // 3)
+# print(10 % 3)
