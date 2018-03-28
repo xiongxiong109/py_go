2f8b95d0e04a4ebcf21a62657597dff70132f4dd
xjq熊剑桥 <jqxiong@Ctrip.com>
2018-03-28 10:00:05 +0800

    loop

---
+++ loop.py
@@ -0,0 +1,18 @@
+# for 循环
+# arr = ["hello", 'hi', 123]
+# for item in arr:
+#     print(item)
+
+# range
+# sum = 0;
+# for i in range(101):
+#     sum += i;
+# print(sum);
+
+# while
+# n = 1;
+# m = 0;
+# while n <= 100:
+#     m += n;
+#     n += 1;
+# print(m);
