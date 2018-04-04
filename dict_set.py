add81094b04f633984293d4a7b90b65bd1fb3826
xjq熊剑桥 <jqxiong@Ctrip.com>
2018-04-04 10:15:56 +0800

    update set

--- dict_set.py
+++ dict_set.py
@@ -18,8 +18,18 @@ envMap = {
 
 envArr = ['dev', 'fat', 'fws', 'beta', 'uat', 'lpt', 'prod', 'prd', 'pro'];
 
-for env in envArr:
-    if (envMap.get(env)):
-        print('found env', env, envMap[env]);
-    else:
-        print('env', env, 'not found');
+# for env in envArr:
+    # print(envMap.get(env)); # 获取不到的时候会返回None
+    # if (envMap.get(env)):
+    #     print('found env', env, envMap[env]);
+    # else:
+    #     print('env', env, 'not found');
+
+# dict是一组key不可变的集合
+# set也是一组key的集合，但不存储value, set 传入一个list, 但是set不是有序的
+envS1 = set(envArr)
+envS2 = set(envArr)
+envS1.add('lpt'); # 添加key值
+envS2.remove('dev'); # 移除key值
+print(envS1 & envS2); # 两组set可以做交集和并集运算
+print(envS1 | envS2);
