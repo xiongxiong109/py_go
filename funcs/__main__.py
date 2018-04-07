a0a742cb4b3f219dd6eff0fd7aff73fc47a7fe2c
xiongxiong109 <444122662@qq.com>
2018-04-07 20:46:57 +0800

    默认参数

---
+++ funcs/__main__.py
@@ -0,0 +1,19 @@
+# from my_abs import pos, abstractFun;
+# from quadratic import quadratic;
+from my_abs import appendA, appendB;
+# 返回第一象限的坐标
+# absPosition = pos(1, 2);
+
+# abstractFun('a'); # 占位函数不做任何事情
+
+# print(absPosition);
+
+# print(quadratic(2, 3, 1));
+# print(quadratic(1, 3, -4));
+
+# print(power(2));
+
+print(appendA());
+print(appendA());
+print(appendB());
+print(appendB());
--- funcs/my_abs.py
+++ funcs/my_abs.py
@@ -1,3 +1,5 @@
+import math
+
 # 用def定义一个函数
 def myAbs(a):
     if not isinstance(a, (int, float)): # 添加数据类型判断
@@ -10,3 +12,17 @@ def pos(x, y):
 def abstractFun(str): # 定义一个空函数, 用pass来占位
     if (str):
         pass
+
+# 给函数参数默认值(默认参数必须指向不可变对象, 否则每次调用时都会记住上一次的变化)
+def power(x, n = 2):
+    return math.pow(x, n);
+
+def appendA(L = []):
+    L.append('END');
+    return L;
+
+def appendB(L = None):
+    if L == None:
+        L = [];
+    L.append('END');
+    return L;
