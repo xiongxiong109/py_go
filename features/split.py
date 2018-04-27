7d36844841eaae986ce96ac1af66e19503938a10
xjq熊剑桥 <jqxiong@Ctrip.com>
2018-04-27 11:18:23 +0800

    test trim

--- features/split.py
+++ features/split.py
@@ -1,5 +1,5 @@
-# 数组切片
-arr = ['a', 1, '2', 'ss'];
+# list切片
+# arr = ['a', 1, '2', 'ss'];
 
 # print(arr[:3])  0 - 3
 
@@ -8,8 +8,51 @@ arr = ['a', 1, '2', 'ss'];
 # 间隔取数
 # print(arr[::2]); # 第三个冒号后面是间隔数
 # 可以利用切片复制一个数组
-newArr = arr[:]
+# newArr = arr[:]
 
-print(newArr)
+# print(newArr)
 
-print(newArr == arr) # 相等?
+# print(newArr == arr) # 相等?
+
+# 利用list切片来实现string的strip()效果
+testStr = "  hello world   ";
+specStr = "hello world";
+# print(testStr.strip() == specStr);
+
+#用一个循环找到前后索引
+
+def trim(str):
+
+    startIdx = -1;
+    isStartReady = False;
+    endIdx = 0;
+    isEndReady = False;
+
+    for char in str:
+        if  str[startIdx + 1] == ' ' and not isStartReady:
+            startIdx += 1;
+        else:
+            isStartReady = True;
+        if str[endIdx - 1] == ' ' and not isEndReady:
+            endIdx -= 1;
+        else:
+            isEndReady = True;
+        if isStartReady and isEndReady:
+            break;
+    return str[startIdx + 1:len(str) + endIdx];
+
+# test
+if trim('hello  ') != 'hello':
+    print('failed!')
+elif trim('  hello') != 'hello':
+    print('failed!')
+elif trim('  hello  ') != 'hello':
+    print('failed!')
+elif trim('  hello  world  ') != 'hello  world':
+    print('failed!')
+elif trim('') != '':
+    print('failed!')
+elif trim('    ') != '':
+    print('failed!')
+else:
+    print('success!')
